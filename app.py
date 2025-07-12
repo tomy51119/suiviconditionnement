2b7fdc12f0ea5fa13cff929c4a760a9eb7c8ac2e
tom51119 <thomas.stasse@gmail.com>
2025-07-12 14:20:04 +0200

    flix final: port 0.0.0.0 pour render

--- app.py
+++ app.py
@@ -175,9 +175,9 @@ def edit(id):
         'ligne': row[2],
         'photo': row[3]
     })
-
 if __name__ == '__main__':
     init_db()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
+
 
